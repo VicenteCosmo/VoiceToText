371014dad82f874819657b4dc1601305ad1ffb41
VicenteCosmo <vicentecosmo55@gmail.com>
2024-11-11 17:59:06 +0100

    voice to text api

--- app.cjs
+++ app.cjs
@@ -1,6 +1,4 @@
 const express = require('express')
-const http = require('http')
-const fs = require('fs')
 const Router = require('./routes.js')
 const bodyParser = require('body-parser')
 
@@ -21,16 +19,3 @@ app.listen(4000, (e)=>{
         console.log('app running...')
     }
 })
-
-// const options = {
-//     key: fs.readFileSync('./certificates/private.key'),
-//     cert: fs.readFileSync('./certificates/certificate.crt')
-// }
-// http.createServer(options, app).listen(4000, (e)=>{
-//     if(e){
-//                 console.log('erro ao ouvir a porta:'+e)
-//             }
-//             else{
-//                 console.log('app running...')
-//             }
-// })
