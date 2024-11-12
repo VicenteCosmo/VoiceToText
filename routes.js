f2366fe68442c97f9e70567a7258e66ff75b2f28
VicenteCosmo <155671006+VicenteCosmo@users.noreply.github.com>
2024-11-12 09:08:27 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -33,60 +33,35 @@ router.post('/', encoder, (req, res)=>{
 } )
 
 
-router.get('/baixar', async (req, res) => { 
-    const query = "SELECT content FROM bzochzffqul3hly6dhy5.voicetext ORDER BY id DESC LIMIT 1"; 
-    DB.query(query, async (err, result) => { 
-        if (err) { 
-        console.log('Erro ao apresentar o texto'); 
-        res.status(500).send('Erro ao apresentar o texto'); } 
-        else { 
-            const content = result[0].content; 
-            console.log(content); 
-            try { const browser = await puppeteer.launch(); 
-                const page = await browser.newPage(); 
-                await page.setContent(`<html><body>${content}</body></html>`); 
-                const buffer = await page.pdf({ format: 'A4' }); 
-                await browser.close(); res.setHeader('Content-Disposition', 'attachment; filename=file.pdf'); 
-                res.setHeader('Content-Type', 'application/pdf'); 
-                res.send(buffer);
-                console.log('pdf gerado')
-             } 
-                catch (e) { 
-                    console.log('Erro ao gerar o PDF:', e); 
-                    res.status(500).send('Erro ao gerar o PDF'); } 
-} 
-})
-});
 
+ router.get('/baixar', (req, res)=>{
 
-// router.get('/baixar', (req, res)=>{
+     const query = "SELECT content FROM bzochzffqul3hly6dhy5.voicetext ORDER BY id DESC LIMIT 1"
+     DB.query(query, (err, result)=>{
+         if(err){
+             console.log('erro ao aprensentar o texto')
+         }
+         else{
+             const content = result[0].content
+             console.log(content)
 
-//     const query = "SELECT content FROM bzochzffqul3hly6dhy5.voicetext ORDER BY id DESC LIMIT 1"
-//     DB.query(query, (err, result)=>{
-//         if(err){
-//             console.log('erro ao aprensentar o texto')
-//         }
-//         else{
-//             const content = result[0].content
-//             console.log(content)
+             pdf.create(content, options).toBuffer((e, buffer)=>{
+                 if(e){
+                     console.log('erro ao gerar o pdf:'+e)
+                 }
+                 else{
+                     console.log('pdf gerado')
 
-//             pdf.create(content, options).toBuffer((e, buffer)=>{
-//                 if(e){
-//                     console.log('erro ao gerar o pdf:'+e)
-//                 }
-//                 else{
-//                     console.log('pdf gerado')
+                     res.setHeader('Content-Disposition', 'attachment ; filename=file.pdf')
+                     res.setHeader('Content-Type', application/pdf)
 
-//                     res.setHeader('Content-Disposition', 'attachment ; filename=file.pdf')
-//                     res.setHeader('Content-Type', application/pdf)
+                     res.send(buffer)
+                 }
+             })
+         }
 
-//                     res.send(buffer)
-//                 }
-//             })
-//         }
-
-//     })
-// })
+     })
+ })
 
 module.exports = router
 
