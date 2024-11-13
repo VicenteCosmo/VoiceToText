bb7e572ee14d18eb0366ac9a8bf613cd5db206d1
VicenteCosmo <155671006+VicenteCosmo@users.noreply.github.com>
2024-11-13 13:08:58 +0100

    Update app.cjs

--- app.cjs
+++ app.cjs
@@ -6,10 +6,14 @@ const app = express()
 
 
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use('/', Router)
-app.use(bodyParser.urlencoded({extended: true}))
+//app.set('view engine', 'ejs')
+//app.use(express.static('public'))
+//app.use('/', Router)
+//app.use(bodyParser.urlencoded({extended: true}))
+
+app.set("views", __dirname + "/views")
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + "public"))
 
 const port = 4001
 
